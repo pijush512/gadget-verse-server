0ac0b08793bf8ee389ebeb3515edce2cf78ff16e
pijush512 <psagorsarker512@gmail.com>
2026-01-17 02:15:42 +0600

    Get Items by Id, Api created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config(); // .env ফাইল ব্যবহারের জন্য
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config(); // .env ফাইল ব্যবহারের জন্য
 
 const app = express();
 const port = 5000;
@@ -10,19 +10,18 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-
 // gadgetVerse
 // CDwXUq874P9jRhW5
 
 // const uri = `mongodb+srv://gadgetVerse:CDwXUq874P9jRhW5@cluster0.5fdvbil.mongodb.net/?appName=Cluster0`
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5fdvbil.mongodb.net/?appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5fdvbil.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,17 +29,23 @@ async function run() {
     // MongoDB Connect
     const database = client.db("gadgetVerse");
     const itemCollection = database.collection("items");
-    
 
-  //  All Item Get
-    app.get('/items', async (req, res) => {
+    //  All Item Get
+    app.get("/items", async (req, res) => {
       const cursor = itemCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    app.get("/items/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }; 
+      const result = await itemCollection.findOne(query);
+      res.send(result);
+    });
+
     // Add Item
-    app.post('/add-item', async (req, res) => {
+    app.post("/add-item", async (req, res) => {
       const newItem = req.body;
       const result = await itemCollection.insertOne(newItem);
       res.send(result);
@@ -48,12 +53,11 @@ async function run() {
 
     console.log("Connected to MongoDB successfully!");
   } finally {
-    
   }
 }
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send("GadgetVere is running!");
 });
 
