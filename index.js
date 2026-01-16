55409c733c24d165752f00136097237ad0c31a27
pijush512 <psagorsarker512@gmail.com>
2026-01-17 05:47:13 +0600

    Somthing is worng  in my code for veployment

--- index.js
+++ index.js
@@ -1,19 +1,83 @@
+// const express = require("express");
+// const cors = require("cors");
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+// require("dotenv").config(); // .env ফাইল ব্যবহারের জন্য
+
+// const app = express();
+// const port = 5000;
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // gadgetVerse
+// // CDwXUq874P9jRhW5
+
+// // const uri = `mongodb+srv://gadgetVerse:CDwXUq874P9jRhW5@cluster0.5fdvbil.mongodb.net/?appName=Cluster0`
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5fdvbil.mongodb.net/?appName=Cluster0`;
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // MongoDB Connect
+//     const database = client.db("gadgetVerse");
+//     const itemCollection = database.collection("items");
+
+//     //  All Item Get
+//     app.get("/items", async (req, res) => {
+//       // const cursor = itemCollection.find();
+//       const result = await itemCollection.find().sort({_id: -1}).toArray();
+//       res.send(result);
+//     });
+
+//     app.get("/items/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const query = { _id: new ObjectId(id) }; 
+//       const result = await itemCollection.findOne(query);
+//       res.send(result);
+//     });
+
+//     // Add Item
+//     app.post("/add-item", async (req, res) => {
+//       const newItem = req.body;
+//       const result = await itemCollection.insertOne(newItem);
+//       res.send(result);
+//     });
+
+//     console.log("Connected to MongoDB successfully!");
+//   } finally {
+//   }
+// }
+// run().catch(console.dir);
+
+// app.get("/", (req, res) => {
+//   res.send("GadgetVere is running!");
+// });
+
+// // app.listen(port, () => {
+// //   console.log(`Server is running at http://localhost:${port}`);
+// // });
+// module.exports = app;
+
+
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require("dotenv").config(); // .env ফাইল ব্যবহারের জন্য
+require("dotenv").config();
 
 const app = express();
-const port = 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// gadgetVerse
-// CDwXUq874P9jRhW5
-
-// const uri = `mongodb+srv://gadgetVerse:CDwXUq874P9jRhW5@cluster0.5fdvbil.mongodb.net/?appName=Cluster0`
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5fdvbil.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -24,43 +88,60 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
-  try {
-    // MongoDB Connect
+// ডাটাবেস কালেকশন ডিফাইন করা (গ্লোবালি)
+let itemCollection;
+
+async function connectDB() {
+  if (!itemCollection) {
+    await client.connect();
     const database = client.db("gadgetVerse");
-    const itemCollection = database.collection("items");
-
-    //  All Item Get
-    app.get("/items", async (req, res) => {
-      const cursor = itemCollection.find();
-      const result = await itemCollection.find().sort({_id: -1}).toArray();
-      res.send(result);
-    });
-
-    app.get("/items/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) }; 
-      const result = await itemCollection.findOne(query);
-      res.send(result);
-    });
-
-    // Add Item
-    app.post("/add-item", async (req, res) => {
-      const newItem = req.body;
-      const result = await itemCollection.insertOne(newItem);
-      res.send(result);
-    });
-
-    console.log("Connected to MongoDB successfully!");
-  } finally {
+    itemCollection = database.collection("items");
+    console.log("Connected to MongoDB");
   }
+  return itemCollection;
 }
-run().catch(console.dir);
+
+// --- API Routes ---
+
+// All Item Get
+app.get("/items", async (req, res) => {
+  try {
+    const collection = await connectDB();
+    const result = await collection.find().sort({ _id: -1 }).toArray();
+    res.json(result); // res.send এর বদলে res.json ব্যবহার করা নিরাপদ
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Single Item Get
+app.get("/items/:id", async (req, res) => {
+  try {
+    const collection = await connectDB();
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) };
+    const result = await collection.findOne(query);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Add Item
+app.post("/add-item", async (req, res) => {
+  try {
+    const collection = await connectDB();
+    const newItem = req.body;
+    const result = await collection.insertOne(newItem);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 app.get("/", (req, res) => {
-  res.send("GadgetVere is running!");
+  res.send("GadgetVerse Server is Active and Running!");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+// Vercel এর জন্য এক্সপোর্ট
+module.exports = app;
